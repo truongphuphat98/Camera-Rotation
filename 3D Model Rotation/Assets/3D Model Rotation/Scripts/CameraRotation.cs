1dc4e295fa2ceb33e57f5b52308ee38183e93d4e
Truong Phu Phat Felix (Zyo) <truongphuphat98@yahoo.fr>
2018-10-23 15:29:50 +0200

    Add Camera-Rotation Project

---
+++ 3D Model Rotation/Assets/3D Model Rotation/Scripts/CameraRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public float cameraSpeed;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.Rotate(0, cameraSpeed * Time.deltaTime, 0);
+	}
+}
